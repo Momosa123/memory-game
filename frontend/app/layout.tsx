7eb74be8dcf7e52bdf6562ca1435ac0f80c8e718
Mouhamadou Sarr <mouhamadoul.sarr@gmail.com>
2025-05-27 07:15:44 +0200

    refactor: replace QueryClientProvider in layout with custom QueryProvider for improved structure and maintainability

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,7 @@
-"use client";
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import { useState } from "react";
+import QueryProvider from "./provider/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,10 +23,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -38,6 +32,6 @@ export default function RootLayout({
           {children}
         </body>
       </html>
-    </QueryClientProvider>
+    </QueryProvider>
   );
 }
